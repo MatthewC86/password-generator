b1d052993011c98c8709e6b9d8bb59bfc6628976
Matthew Courtney <cnymhw86@gmail.com>
2022-11-09 16:35:43 -0500

    established character limit for pw reqs in js

--- script.js
+++ script.js
@@ -8,7 +8,28 @@ let passwordOptions = {
     upperCase: "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
   };
   
- 
+ var generateBtn  =document.querySelector("#generate");
+
+ function generatePassword() {
+
+    var userInput = window.prompt("How many characters would you like?")
+
+    var passwordLength = parseInt(userInput)
+    //Check to ensure a number is being entered.
+    if (isNaN(passwordLength)) {
+        alert("That's not a number!")
+    //User returned for NaN.
+    return null;
+    }
+
+    //Next, we write a statement to ensure is at least 8 and no more than 128
+
+    if (passwordLength < 8 || passwordLength > 128) {
+        alert("Must be between 8 and 128 characters")
+        return null;
+    }
+
+ }
   
 
   

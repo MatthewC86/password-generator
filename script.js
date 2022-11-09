ddb32d57df2e4599f1a652889c03174432470f9a
Matthew Courtney <cnymhw86@gmail.com>
2022-11-09 17:02:42 -0500

    added user input values to js

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 // passwordOptions contains all necessary string data needed to generate the password
 
 
-let passwordOptions = {
-    num: "1234567890",
-    specialChar: "!@#$%&'()*+,^-./:;<=>?[]_`{~}|",
-    lowerCase: "abcdefghijklmnopqrstuvwxyz",
-    upperCase: "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-  };
+//let passwordOptions = {
+   // num: "1234567890",
+   // specialChar: "!@#$%&'()*+,^-./:;<=>?[]_`{~}|",
+    //lowerCase: "abcdefghijklmnopqrstuvwxyz",
+   // upperCase: "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+  //};
   
- var generateBtn  =document.querySelector("#generate");
+ var generateBtn = document.querySelector("#generate");
 
  function generatePassword() {
 
@@ -28,8 +28,32 @@ let passwordOptions = {
         alert("Must be between 8 and 128 characters")
         return null;
     }
-
+    //Prompt for options.
+    var userNumbers = confirm("Would you like to include numbers?");
+    var userSymbols = confirm("Would you like to include symbols?");
+    var userLowerCase = confirm("Would you like to include lowercase letters?");
+    var userUpperCase = confirm("Would you like to include uppercase letters?");
+    
  }
+
+
+
+
+
+
+
+
+ //Password going to text box
+    function writePassword(){
+        var password = generatePassword();
+        var passwordText = document.querySelector("#text")
+
+        passwordText.value = password;
+    }
+
+
+ //Add listener to button
+ generateBtn.addEventListener("click", writePassword);
   
 
   

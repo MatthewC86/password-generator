a082faa5698b577ba724b398ca5ac84bf915855a
Matthew Courtney <cnymhw86@gmail.com>
2022-11-09 16:23:21 -0500

    finished html and assign var to js

---
+++ script.js
@@ -0,0 +1,14 @@
+// passwordOptions contains all necessary string data needed to generate the password
+
+
+let passwordOptions = {
+    num: "1234567890",
+    specialChar: "!@#$%&'()*+,^-./:;<=>?[]_`{~}|",
+    lowerCase: "abcdefghijklmnopqrstuvwxyz",
+    upperCase: "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+  };
+  
+ 
+  
+
+  
